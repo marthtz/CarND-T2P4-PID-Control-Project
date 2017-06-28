1a7a4de49780a67bb5abbe5f6841112a2f02209a
Martin Hintz <mhintz@posteo.net>
2017-06-28 22:49:02 +0930

    Implemented PID controller with arguments

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,31 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp, double Ki, double Kd)
+{
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+  this->d_error = 0;
+  this->p_error = 0;
+  this->i_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+  this->d_error = cte - this->p_error;
+  this->p_error = cte;
+  this->i_error += cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+  double total_error;
+
+  total_error = (this->Kp * this->p_error) +
+                (this->Kd * this->d_error) + 
+                (this->Ki * this->i_error);
+
+  return total_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <uWS/uWS.h>
 #include <iostream>
+#include <string>
 #include "json.hpp"
 #include "PID.h"
 #include <math.h>
@@ -28,12 +29,18 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+int main(int argc, char *argv[])
 {
   uWS::Hub h;
 
   PID pid;
   // TODO: Initialize the pid variable.
+
+  double Kp_init = std::stod(argv[1]); //0.2;
+  double Ki_init = std::stod(argv[2]); // 0.0005;//0.004;
+  double Kd_init = std::stod(argv[3]); //2.0;//3.0;
+
+  pid.Init(Kp_init, Ki_init, Kd_init);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -48,8 +55,8 @@ int main()
         if (event == "telemetry") {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          //double speed = std::stod(j[1]["speed"].get<std::string>());
+          //double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
@@ -57,7 +64,8 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
